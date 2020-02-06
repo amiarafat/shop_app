f22b4dc7373e66831409fa4690155237efd1b88a
Hossain Md. Arafat <arafat@Hossains-MacBook-Pro.local>
2020-02-06 18:10:43 +0600

    4th commit with drawer items with widgets

--- lib/main.dart
+++ lib/main.dart
@@ -47,8 +47,80 @@ class _HomePageState extends State<HomePage> {
               currentAccountPicture: GestureDetector(
                 child: new CircleAvatar(
                   backgroundColor: Colors.grey,
-                  child: Icon(Icons.person, color: Colors.white,size: 40,),
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 40,
+                  ),
                 ),
+              ),
+              decoration: new BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+//            Body
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home, color: Colors.red,),
+
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(Icons.account_circle, color: Colors.red,),
+
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket , color: Colors.red,),
+
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard, color: Colors.red,),
+
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Favourits'),
+                leading: Icon(Icons.favorite, color: Colors.red,),
+
+              ),
+            ),
+
+            Divider(),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings, color: Colors.red,),
+
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help, color: Colors.red,),
+
               ),
             ),
 
